4c5f355ffd71d455cd8876f6536ad3395f677a5f
Dmitry Mozzherin <dmozzherin@gmail.com>
2010-02-25 21:42:20 -0500

    added reports

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "config", "boot"))
 require "selenium/client"
 require "selenium/rspec/spec_helper"
+require "selenium/rspec/reporting/selenium_test_report_formatter"
 
 Spec::Runner.configure do |config|
  
